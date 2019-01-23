e4aef152cdbc1e8ca6777c64a50bdae23b654a27
Maximo Mena <maximo.mena@earlywarning.com>
2019-01-22 23:34:14 -0700

    Updates

---
+++ precache-manifest.7dddbe97b0550463bb4a75ba5b099142.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84239915bb79ea69d4ba",
+    "url": "/react-memory-game/static/css/main.28f21a2f.chunk.css"
+  },
+  {
+    "revision": "84239915bb79ea69d4ba",
+    "url": "/react-memory-game/static/js/main.84239915.chunk.js"
+  },
+  {
+    "revision": "50a6d858bb4c800fe862",
+    "url": "/react-memory-game/static/js/1.50a6d858.chunk.js"
+  },
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "c6f65f4b7d95e4c1e3f57c5f9c751e32",
+    "url": "/react-memory-game/index.html"
+  }
+];
